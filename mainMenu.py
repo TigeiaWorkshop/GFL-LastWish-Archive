f8e013cc2c01dfc5d8d46f731433318cfeb017da
yudong <yoshino1347716570@gmail.com>
2019-10-20 05:40:56 +0800

    add function to load yaml file

--- mainMenu.py
+++ mainMenu.py
@@ -7,23 +7,27 @@ import os
 import yaml
 pygame.init()
 
-#加载主菜单文字
-my_font = pygame.font.SysFont("font/font.ttf", 200)
-lang_yaml = open("lang/zh_cn.yaml", "r", encoding='utf-8')
-lang_yaml = lang_yaml.read()
+#加载设置
+with open("setting.yaml", "r", encoding='utf-8') as f:
+    setting = yaml.load(f.read(),Loader=yaml.FullLoader)
+    window_x = setting['Screen_size_x']
+    window_y =  setting['Screen_size_y']
+    lang_file = setting['Language']
 
-text_title = my_font.render("少女前线", True, (255, 255, 255))
-text_continue = my_font.render("继续游戏", True, (0,0,0))
-text_chooseChapter = my_font.render("选择章节", True, (0,0,0))
-text_setting = my_font.render("设置", True, (0,0,0))
-text_dlc = my_font.render("可下载内容", True, (0,0,0))
-text_wrokshop = my_font.render("创意工坊", True, (0,0,0))
-text_exit = my_font.render("退出", True, (0,0,0))
+#加载主菜单文字
+with open("lang/"+lang_file+".yaml", "r", encoding='utf-8') as f:
+    lang_cn = yaml.load(f.read(),Loader=yaml.FullLoader)
+    my_font =pygame.font.SysFont('simsunnsimsun',60)
+    text_title = my_font.render(lang_cn['main_menu']['text_title'], True, (255, 255, 255))
+    text_continue = my_font.render(lang_cn['main_menu']['text_continue'], True, (255, 255, 255))
+    text_chooseChapter = my_font.render(lang_cn['main_menu']['text_chooseChapter'], True, (255, 255, 255))
+    text_setting = my_font.render(lang_cn['main_menu']['text_setting'], True, (255, 255, 255))
+    text_dlc = my_font.render(lang_cn['main_menu']['text_dlc'], True, (255, 255, 255))
+    text_wrokshop = my_font.render(lang_cn['main_menu']['text_wrokshop'], True, (255, 255, 255))
+    text_exit = my_font.render(lang_cn['main_menu']['text_exit'], True, (255, 255, 255))
 
 # 创建窗口
-window_x = 1920
-window_y = 1080
-screen = pygame.display.set_mode((1920, 1080), 0, 32)
+screen = pygame.display.set_mode((window_x, window_y), 0, 32)
 pygame.display.set_caption("Girls frontline-Last Wish") #窗口标题
 
 #加载背景
@@ -46,7 +50,6 @@ while True:
     screen.blit(loading_img, (0,0))
     screen.blit(my_font.render(str(percent_of_img_loaded), True, (255, 255, 255)), (10,10))
     pygame.display.update()
-
 background_img_id = 0
 
 # 游戏主循环
@@ -59,7 +62,13 @@ while True:
 
     #角色动作
     screen.blit(background_img_list[background_img_id], (0,0))
-    screen.blit(text_title, (100,100))
+    screen.blit(text_title, (1300,100))
+    screen.blit(text_continue, (1500,250))
+    screen.blit(text_chooseChapter, (1500,350))
+    screen.blit(text_setting, (1500,450))
+    screen.blit(text_dlc, (1500,550))
+    screen.blit(text_wrokshop, (1500,650))
+    screen.blit(text_exit, (1500,750))
     background_img_id += 1
     if background_img_id == 374:
         background_img_id = 0
