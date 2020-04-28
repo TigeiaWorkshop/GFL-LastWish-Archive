89ab208564195e7eaa1ce756cf7bbbe3f1d9e8ac
yudonglin <yoshino1347716570@gmail.com>
2020-04-28 16:35:17 -0700

    对setup进行调整

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import os
 import shutil
 
 #python setup.py build_ext
-#pyinstaller -F -i main.ico main.py
+#pyinstaller -i main.ico main.py
 
 #生成c和pyd文件
 all_Zero2_py_files = glob.glob(r'Zero2/*.py')
@@ -21,7 +21,7 @@ if len(all_Zero2_pyd_files)>0:
     for i in range(len(all_Zero2_pyd_files)):
         os.remove(all_Zero2_pyd_files[i])
 #移动pyd文件
-all_Zero2_pyd_files = glob.glob(r'build/lib.win-amd64-3.7/*.pyd')
+all_Zero2_pyd_files = glob.glob(r'build/lib.win-amd64-3.8/*.pyd')
 for i in range(len(all_Zero2_pyd_files)):
     shutil.move(all_Zero2_pyd_files[i],"Zero2_pyd/")
 #删除build文件夹
