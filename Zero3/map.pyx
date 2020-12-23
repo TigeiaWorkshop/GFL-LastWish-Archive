454eccb1f5e48bf65efe34c651a410a8fccf803f
yudonglin <yoshino1347716570@gmail.com>
2020-12-22 22:40:51 -0800

    对引擎架构进行优化

--- Zero3/map.pyx
+++ Zero3/map.pyx
@@ -1,114 +1,11 @@
 # cython: language_level=3
 import numpy
-import pygame
-from Zero3.basic import addDarkness, loadImg, randomInt, resizeImg, loadConfig
+from Zero3.basic import randomInt, loadConfig
 from Zero3.mapModule import *
 
 _MAP_ENV_IMAGE = None
 #方块数据
 _BLOCKS_DATABASE = loadConfig("Data/blocks.yaml","blocks")
-
-#地图场景模块
-class EnvImagesManagement:
-    def __init__(self,theMap,ornamentationData,bgImgName,theWidth,darkMode,darkness=150):
-        self.__ENV_IMAGE_DICT_ORIGINAL = {}
-        self.__ENV_IMAGE_DICT_ORIGINAL_DARK = None
-        self.__ENV_IMAGE_DICT = {}
-        self.__ENV_IMAGE_DICT_DARK = None
-        self.__ORNAMENTATION_IMAGE_DICT = {}
-        self.__ORNAMENTATION_IMAGE_DICT_DARK = None
-        self.__BACKGROUND_IMAGE = loadImg("Assets/image/dialog_background/{}".format(bgImgName),ifConvertAlpha=False).convert() if bgImgName != None else None
-        self.__BACKGROUND_SURFACE = None
-        self.__MAP_BUFFER_SURFACE = None
-        cdef list all_images_needed = []
-        for y in range(len(theMap)):
-            for x in range(len(theMap[y])):
-                if theMap[y][x].name not in all_images_needed:
-                    all_images_needed.append(theMap[y][x].name)
-        #加载背景图片
-        for fileName in all_images_needed:
-            try:
-                self.__ENV_IMAGE_DICT_ORIGINAL[fileName] = loadImg("Assets/image/environment/block/"+fileName+".png")
-                self.__ENV_IMAGE_DICT[fileName] = resizeImg(self.__ENV_IMAGE_DICT_ORIGINAL[fileName],(theWidth,None))
-            except BaseException:
-                raise Exception('ZeroEngine-Error: An map-block called '+fileName+' cannot find its image in the folder.')
-        #加载场地设施的图片
-        for ornamentation in ornamentationData:
-            #--篝火--
-            if ornamentation.type == "campfire":
-                if "campfire" not in self.__ORNAMENTATION_IMAGE_DICT:
-                    self.__ORNAMENTATION_IMAGE_DICT["campfire"] = []
-                    for i in range(1,12):
-                        self.__ORNAMENTATION_IMAGE_DICT["campfire"].append(loadImg("Assets/image/environment/campfire/{}.png".format(i)))
-            elif ornamentation.type not in self.__ORNAMENTATION_IMAGE_DICT:
-                self.__ORNAMENTATION_IMAGE_DICT[ornamentation.type] = {}
-                self.__ORNAMENTATION_IMAGE_DICT[ornamentation.type][ornamentation.image] = loadImg("Assets/image/environment/decoration/"+ornamentation.image+".png")
-            elif ornamentation.image not in self.__ORNAMENTATION_IMAGE_DICT[ornamentation.type]:
-                self.__ORNAMENTATION_IMAGE_DICT[ornamentation.type][ornamentation.image] = loadImg("Assets/image/environment/decoration/"+ornamentation.image+".png")
-        #如果是夜战模式
-        if darkMode:
-            self.__ENV_IMAGE_DICT_ORIGINAL_DARK = {}
-            for img,value in self.__ENV_IMAGE_DICT_ORIGINAL.items():
-                self.__ENV_IMAGE_DICT_ORIGINAL_DARK[img] = addDarkness(value,darkness)
-            self.__ENV_IMAGE_DICT_DARK = {}
-            for img,value in self.__ENV_IMAGE_DICT.items():
-                self.__ENV_IMAGE_DICT_DARK[img] = addDarkness(value,darkness)
-            self.__ORNAMENTATION_IMAGE_DICT_DARK = {}
-            for key,value in self.__ORNAMENTATION_IMAGE_DICT.items():
-                if key != "campfire":
-                    self.__ORNAMENTATION_IMAGE_DICT_DARK[key] = {}
-                    for key2,value2 in value.items():
-                        self.__ORNAMENTATION_IMAGE_DICT_DARK[key][key2] = addDarkness(value2,darkness)
-                elif "campfire" not in self.__ORNAMENTATION_IMAGE_DICT_DARK:
-                    self.__ORNAMENTATION_IMAGE_DICT_DARK["campfire"] = {}
-                    self.__ORNAMENTATION_IMAGE_DICT_DARK["campfire"]["campfire"] = (addDarkness(self.__ORNAMENTATION_IMAGE_DICT["campfire"][-1],darkness))
-    def resize(self,newWidth):
-        for key in self.__ENV_IMAGE_DICT:
-            self.__ENV_IMAGE_DICT[key] = resizeImg(self.__ENV_IMAGE_DICT_ORIGINAL[key],(newWidth, None))
-        if self.__ENV_IMAGE_DICT_ORIGINAL_DARK != None:
-            for key in self.__ENV_IMAGE_DICT_DARK:
-                self.__ENV_IMAGE_DICT_DARK[key] = resizeImg(self.__ENV_IMAGE_DICT_ORIGINAL_DARK[key],(newWidth,None))
-    def get_env_image(self,key,darkMode):
-        try:
-            if darkMode:
-                return self.__ENV_IMAGE_DICT_DARK[key]
-            else:
-                return self.__ENV_IMAGE_DICT[key]
-        except BaseException:
-            print('ZeroEngine-Warning: Cannot find block image "{}", we will try to load it for you right now, but please by aware.'.format(key))
-            imgTmp = loadImg("Assets/image/environment/block/"+key+".png")
-            self.__ENV_IMAGE_DICT_ORIGINAL[key] = imgTmp
-            self.__ENV_IMAGE_DICT[key] = resizeImg(imgTmp,(self.perBlockWidth,None))
-            if self.__ENV_IMAGE_DICT_ORIGINAL_DARK != None:
-                imgTmp = addDarkness(imgTmp,150)
-                self.__ENV_IMAGE_DICT_ORIGINAL_DARK[key] = imgTmp
-                self.__ENV_IMAGE_DICT_DARK[key] = resizeImg(imgTmp,(self.perBlockWidth,None))
-    def get_ornamentation_image(self,ornamentationType,key,darkMode,darkness=150):
-        try:
-            if darkMode:
-                return self.__ORNAMENTATION_IMAGE_DICT_DARK[ornamentationType][key]
-            else:
-                return self.__ORNAMENTATION_IMAGE_DICT[ornamentationType][key]
-        #如果图片没找到
-        except BaseException:
-            print('ZeroEngine-Warning: Cannot find ornamentation image "{0}" in type "{1}", we will try to load it for you right now, but please by aware.'.format(key,ornamentationType))
-            imgTmp = loadImg("Assets/image/environment/decoration/"+key+".png")
-            self.__ORNAMENTATION_IMAGE_DICT[ornamentationType][key] = imgTmp
-            if self.__ORNAMENTATION_IMAGE_DICT_DARK != None:
-                self.__ORNAMENTATION_IMAGE_DICT_DARK[ornamentationType][key] = addDarkness(imgTmp,darkness)
-    #获取当前装饰物种类的数量
-    def get_ornamentation_num(self,ornamentationType):
-        return len(self.__ORNAMENTATION_IMAGE_DICT[ornamentationType])
-    def get_new_background_image(self,screen_size,map_size):
-        if self.__BACKGROUND_IMAGE != None:
-            self.__BACKGROUND_SURFACE = resizeImg(self.__BACKGROUND_IMAGE,screen_size)
-        else:
-            self.__BACKGROUND_SURFACE = pygame.Surface(screen_size).convert()
-        self.__MAP_BUFFER_SURFACE = pygame.Surface(map_size,flags=pygame.SRCALPHA).convert_alpha()
-        return self.__MAP_BUFFER_SURFACE
-    def display_background_surface(self,screen,pos):
-        screen.blit(self.__BACKGROUND_SURFACE,(0,0))
-        screen.blit(self.__MAP_BUFFER_SURFACE,pos)
 
 #地图模块
 class MapObject:
--- Zero3/module.py
+++ Zero3/module.py
@@ -1,15 +1,13 @@
 # cython: language_level=3
 import glob
 import random
-from sys import exit
-from Zero3.font import *
+from Zero3.controller import *
 import time
 from tkinter import Tk
 
 #游戏对象接口
 class GameObject:
-    def __init__(self,img,x,y):
-        self.img = img
+    def __init__(self,x,y):
         self.x = x
         self.y = y
     def get_pos(self):
@@ -18,9 +16,18 @@ class GameObject:
 #图形接口
 class ImageInterface(GameObject):
     def __init__(self,img,x,y,width,height):
-        GameObject.__init__(self,img,x,y)
+        GameObject.__init__(self,x,y)
+        self.img = img
         self.width = width
         self.height = height
+    def set_alpha(self,value):
+        self.img.set_alpha(value)
+    def get_alpha(self):
+        return self.img.get_alpha()
+    def get_width(self):
+        return self.width
+    def get_height(self):
+        return self.height
 
 #高级图形类
 class ImageSurface(ImageInterface):
@@ -40,14 +47,6 @@ class ImageSurface(ImageInterface):
         screen.blit(pygame.transform.scale(self.img,(round(self.width),round(self.height))),(self.x,self.y))
     def display(self,screen,local_x=0,local_y=0):
         screen.blit(pygame.transform.scale(self.img, (round(self.width),round(self.height))),(self.x+local_x,self.y+local_y))
-    def set_alpha(self,value):
-        self.img.set_alpha(value)
-    def get_alpha(self):
-        return self.img.get_alpha()
-    def get_width(self):
-        return self.width
-    def get_height(self):
-        return self.height
     def rotate(self,angle):
         self.img = pygame.transform.rotate(self.img,angle)
     def flip(self,vertical=False,horizontal=False):
@@ -96,42 +95,14 @@ class DynamicImageSurface(ImageSurface):
     def ifToward(self):
         return self.__towardTargetPos
 
-#画面更新控制器
-class DisplayController:
-    def __init__(self,fps):
-        self.fps = fps
-        self.__clock = pygame.time.Clock()
-    def flip(self,pump=False):
-        self.__clock.tick(self.fps)
-        controller.display()
-        if pump == True:
-            pygame.event.pump()
-        pygame.display.flip()
-    def update(self,rectangle=None,pump=False):
-        self.__clock.tick(self.fps)
-        controller.display()
-        if pump == True:
-            pygame.event.pump()
-        if rectangle == None:
-            pygame.display.flip()
-        else:
-            pygame.display.update(rectangle)
-    def set_caption(self,title):
-        pygame.display.set_caption(title)
-    def set_icon(self,path):
-        pygame.display.set_icon(pygame.image.load(os.path.join(path)))
-    def get_width(self):
-        return get_setting("Screen_size_x")
-    def get_height(self):
-        return get_setting("Screen_size_y")
-    def get_size(self):
-        return self.get_width(),self.get_height()
-    def quit(self):
-        #退出游戏
-        exit()
-
-#帧率控制器
-display = DisplayController(get_setting("FPS"))
+#进度条
+class ProgressBar(ImageInterface):
+    def __init__(self,x,y,max_width,height,color):
+        ImageInterface.__init__(self,None,x,y,max_width,height)
+        self.percentage = 0
+        self.color = findColorRGBA(color)
+    def draw(self,screen):
+        pygame.draw.rect(screen,self.color,(self.x,self.y,self.width*self.percentage,self.height))
 
 #环境系统
 class WeatherSystem:
@@ -300,7 +271,7 @@ class ApSystem:
 #按钮
 class Button(GameObject):
     def __init__(self,path,x,y):
-        GameObject.__init__(self,None,x,y)
+        GameObject.__init__(self,x,y)
         self.img = pygame.image.load(os.path.join(path)).convert_alpha() if isinstance(path,str) else path
         self.img2 = None
         self.hoverEventTriggered = False
@@ -357,76 +328,6 @@ class ButtonWithFadeInOut(Button):
         self.img2 = self.img.copy()
         self.img.set_alpha(alphaWhenNotHovered)
 
-#手柄控制组件
-class SingleJoystick:
-    def __init__(self):
-        if pygame.joystick.get_init() == False:
-            pygame.joystick.init()
-        self.inputController = None
-        self.update_device()
-    def update_device(self):
-        if self.inputController == None and pygame.joystick.get_count() > 0:
-            self.inputController = pygame.joystick.Joystick(0)
-            self.inputController.init()
-            print("ZeroEngine: Joystick is detected and initialized successfully.")
-        elif self.inputController != None:
-            if pygame.joystick.get_count() == 0:
-                self.inputController = None
-            elif self.inputController.get_id() != pygame.joystick.Joystick(0).get_id():
-                self.inputController = pygame.joystick.Joystick(0)
-                self.inputController.init()
-                print("ZeroEngine: Joystick changed! New joystick is detected and initialized successfully.")
-    def get_init(self):
-        if self.inputController != None:
-            return self.inputController.get_init()
-        else:
-            return False
-    def get_button(self,buttonId):
-        if self.inputController != None and self.inputController.get_init() == True:
-            return self.inputController.get_button(buttonId)
-        else:
-            return None
-    def get_axis(self,buttonId):
-        if self.inputController != None and self.inputController.get_init() == True:
-            return self.inputController.get_axis(buttonId)
-        else:
-            return 0
-
-#输入管理组件
-class GameController:
-    def __init__(self,mouse_icon_width,speed,custom=False):
-        self.joystick = SingleJoystick()
-        if custom == True:
-            pygame.mouse.set_visible(False)
-            self.iconImg = pygame.transform.scale(pygame.image.load(os.path.join("Assets/image/UI/","mouse_icon.png")).convert_alpha(),(int(mouse_icon_width),int(mouse_icon_width*1.3)))
-        else:
-            self.iconImg = None
-        self.mouse_x,self.mouse_y = pygame.mouse.get_pos()
-        self.movingSpeed = speed
-    def display(self,screen=None):
-        self.joystick.update_device()
-        self.mouse_x,self.mouse_y = pygame.mouse.get_pos()
-        if self.joystick.inputController != None:
-            if self.joystick.get_axis(0)>0.1 or self.joystick.get_axis(0)<-0.1:
-                self.mouse_x += int(self.movingSpeed*round(self.joystick.get_axis(0),1))
-            if self.joystick.get_axis(1)>0.1 or self.joystick.get_axis(1)<-0.1:
-                self.mouse_y += int(self.movingSpeed*round(self.joystick.get_axis(1),1))
-            pygame.mouse.set_pos((self.mouse_x,self.mouse_y))
-        if self.iconImg != None and screen != None:
-            screen.blit(self.iconImg,(self.mouse_x,self.mouse_y))
-    def get_event(self,pygame_events=None):
-        if pygame_events == None:
-            pygame_events = pygame.event.get()
-        for event in pygame_events:
-            if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1 or event.type == pygame.JOYBUTTONDOWN and self.joystick.get_button(0) == 1:
-                return "comfirm"
-        return None
-    def get_pos(self):
-        return self.mouse_x,self.mouse_y
-
-#控制器输入组件初始化
-controller = GameController(get_setting("MouseIconWidth"),get_setting("MouseMoveSpeed"))
-
 #对话框基础模块
 class DialogInterface:
     def __init__(self,img,fontSize):
@@ -444,12 +345,11 @@ class DialogInterface:
         self.narrator = narrator
 
 #对话框和对话框内容
-class DialogBox(DialogInterface):
+class DialogBox(DialogInterface,GameObject):
     def __init__(self,imgPath,width,height,x,y,fontSize):
         DialogInterface.__init__(self,pygame.transform.scale(pygame.image.load(os.path.join(imgPath)).convert_alpha(),(int(width),int(height))),fontSize)
+        GameObject.__init__(self,x,y)
         self.__surface = None
-        self.x = x
-        self.y = y
         self.deafult_x = x
         self.deafult_y = y
         self.txt_x = fontSize
@@ -524,8 +424,9 @@ class DialogBox(DialogInterface):
         self.__flipped = not self.__flipped
 
 #输入框Interface，请勿实体化
-class InputBoxInterface:
+class InputBoxInterface(GameObject):
     def __init__(self,x,y,font_size,txt_color,default_width):
+        GameObject.__init__(self,x,y)
         self.FONTSIZE = font_size
         self.FONT = createFont(self.FONTSIZE)
         self.default_width = default_width
@@ -537,8 +438,6 @@ class InputBoxInterface:
         self._text = None
         self._holder = self.FONT.render("|",get_fontMode(),self.txt_color)
         self.holderIndex = 0
-        self.x = x
-        self.y = y
         self.needSave = False
     def get_width(self):
         return self.input_box.w
@@ -937,12 +836,11 @@ class Console(SingleLineInputBox):
 console = Console(0,0)
 
 #gif图片管理
-class GifObject:
+class GifObject(GameObject):
     def __init__(self,imgList,x,y,width,height,updateGap):
+        GameObject.__init__(self,x,y)
         self.imgList = imgList
         self.imgId = 0
-        self.x = x
-        self.y = y
         self.width = int(width)
         self.height = int(height)
         self.updateGap = updateGap
@@ -1046,12 +944,3 @@ class SoundManagement:
     def set_volume(self,volume):
         for i in range(len(self.__sounds_list)):
             self.__sounds_list[i].set_volume(volume)
-
-#进度条
-class ProgressBar(ImageInterface):
-    def __init__(self,x,y,max_width,height,color):
-        ImageInterface.__init__(self,None,x,y,max_width,height)
-        self.percentage = 0
-        self.color = findColorRGBA(color)
-    def draw(self,screen):
-        pygame.draw.rect(screen,self.color,(self.x,self.y,self.width*self.percentage,self.height))
